5dcd4e1200b21330172e0bcb6a603f800af0be2f
myjhye <t01087633799@gmail.com>
2023-11-18 20:31:24 +0900

    주석 -> PantsPage

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+// Props 타입 정의
 type Props = {
     params: {
         slug: string;
@@ -7,6 +8,19 @@ type Props = {
 export default function PantsPage({params}: Props) {
 
     return (
+        // slug 사용해 페이지 제목을 렌더링
         <h1>{params.slug} pants page</h1>
     )
+}
+
+// 초기 데이터 생성 함수 정의
+export function generateStaticParams() {
+
+    // 제품 목록 배열
+    const products = ['pants', 'skirt'];
+
+    return products.map(product => ({
+        // 각 제품에 대한 slug 생성
+        slug: product
+    }))
 }
--- src/app/products/pants/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function PantsPage() {
-
-    return (
-        <h1>pants page</h1>
-    )
-}
